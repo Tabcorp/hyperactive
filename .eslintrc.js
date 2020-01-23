28130baccaa1a00490b18ea4aa1f69d5e467e0ee
Couch, Dean <Dean.Couch@tabcorp.com.au>
2020-01-23 13:39:05 +1100

    adding decaffeination tools

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "plugins": [
+    "mocha"
+  ],
+  "extends": ['@tabdigital/eslint-config-node'],
+  "rules": {
+    "consistent-return": "off",
+    "max-len": "off"
+  },
+  "overrides": [
+    {
+      "files": [
+        "test/**/*.js"
+      ],
+      "env": {
+        "mocha": true
+      },
+      "rules": {
+        "no-undef": "off",
+        "import/no-dynamic-require": "off",
+        "no-unused-expressions": "off",
+        "import/no-extraneous-dependencies": "off",
+        "global-require": "off"
+      }
+    }
+  ]
+}
